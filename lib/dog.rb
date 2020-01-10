911773ccd9fbe567067f6610acc9da2f1a257363
Victoria Curl <victoriacurl9@gmail.com>
2020-01-10 22:17:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+ def breed=(dogs_breed)
+   @this_dogs_breed = dogs_breed
+ end
+ def breed
+   @this_dogs_breed
+ end
+ end
+ lassie.breed = "Collie"
